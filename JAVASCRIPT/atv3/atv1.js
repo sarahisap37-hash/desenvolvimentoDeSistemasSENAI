ccb8468efe9e3282220f501ebfee8916f40602fd
Sarah Isa <sarahisap37@gmail.com>
2025-09-03 17:02:08 -0300

    1

---
+++ JAVASCRIPT/atv3/atv1.js
@@ -0,0 +1,30 @@
+function atv1()
+{
+var filmes = [];
+console.log(filmes)
+
+filmes[0]="Barbie dreamhouse"
+filmes[1]="Para todos os garotos que eu já amei"
+filmes[2]="Casa monstro"
+console.log=(filmes)
+}
+
+function atv2()
+{
+var numeros = []
+console.log(numeros)
+
+for(i=0; i<=2; i++){
+    var numero = Number(prompt(`Digite o seu ${i+1}º numero:`));
+        numero.push(numero)
+
+}
+
+for(i = 2; i >=0; i--){
+
+console.log(numero[i])
+    
+}
+
+
+}
